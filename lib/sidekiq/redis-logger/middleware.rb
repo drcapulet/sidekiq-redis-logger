521160e9b8f239a42711f464595114545eff2145
Alex Coomans <alex@alexcoomans.com>
2012-12-29 22:32:25 -0700

    Fix missing ::

--- lib/sidekiq/redis-logger/middleware.rb
+++ lib/sidekiq/redis-logger/middleware.rb
@@ -2,7 +2,7 @@ module Sidekiq
   module RedisLogger
     class Middleware
       def call(worker, item, queue)
-        SidekiqRedisLogger::Logger.with_jid_context(item["jid"]) do
+        Sidekiq::RedisLogger::Logger.with_jid_context(item["jid"]) do
           yield
         end
       end
